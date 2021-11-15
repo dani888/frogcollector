5410992e5ad8e972a9234c24a20e42df123ec612
Daniel Werminghausen <daniel.werminghausen.com>
2021-11-14 20:19:24 -0500

    add view page

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+from .models import Frog
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
@@ -11,17 +13,22 @@ def about(request):
   return render(request, 'about.html')
 
 def frogs_index(request):
+  frogs = Frog.objects.all()
   return render(request, 'frogs/index.html', { 'frogs': frogs })
 
-class Frog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+def frogs_detail(request, frog_id):
+  frog = Frog.objects.get(id=frog_id)
+  return render(request, 'frogs/detail.html', { 'frog': frog })
 
-frogs = [
-  Frog('Lolo', 'tabby', 'foul little demon', 3),
-  Frog('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Frog('Raven', 'black tripod', '3 legged frog', 4)
-]
+# class Frog:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
+
+# frogs = [
+#   Frog('Lolo', 'tabby', 'foul little demon', 3),
+#   Frog('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#   Frog('Raven', 'black tripod', '3 legged frog', 4)
+# ]
